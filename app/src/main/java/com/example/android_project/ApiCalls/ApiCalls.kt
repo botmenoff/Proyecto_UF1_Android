98c5833dbadf7a816ca43be65049b15e005a3fd6
Ferran <marvenferry60@gmail.com>
2023-12-14 19:27:40 +0100

    Funcion para hacer llamadas a una API

---
+++ app/src/main/java/com/example/android_project/ApiCalls/ApiCalls.kt
@@ -0,0 +1,56 @@
+package com.example.android_project.ApiCalls
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import okhttp3.Headers
+
+class ApiCalls {
+
+    // Headers por defecto de la api
+    private val defaultHeaders: ArrayList<Pair<String, String>> = arrayListOf(
+        "Authorization" to "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTllYjlkNzk1YmZiYjMwY2FhODRjYTMxNmFiODk2YyIsInN1YiI6IjY1NmUwMWIxNGE0YmY2MDEyMDI4ZGUyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UrQqa_FuEXJL8zkvPtu2OcNb47MxY3Nn55R6nYyP92M",
+        "Content-Type" to "application/json"
+    )
+
+    // Función para realizar una llamada GET a una API
+    fun fetchDataFromApi(apiUrl: String, onComplete: (String) -> Unit) {
+        // Launch a coroutine on the IO dispatcher
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                // Gestionar conexión HTTP
+                val client = OkHttpClient()
+
+                // Crear objeto Headers y añadir los encabezados por defecto
+                val headersBuilder = Headers.Builder()
+                for ((name, value) in defaultHeaders) {
+                    headersBuilder.add(name, value)
+                }
+                val requestHeaders = headersBuilder.build()
+
+                // Hacer un objeto request utilizando Request.Builder() y especificando la URL
+                val request = Request.Builder()
+                    .url(apiUrl)
+                    .headers(requestHeaders) // Añadir headers
+                    .build()
+
+                // Hacemos la petición y obtenemos la respuesta
+                val response: okhttp3.Response = client.newCall(request).execute()
+
+                // Transformamos el objeto de tipo Response a un String
+                val responseData = response.body?.string() ?: ""
+
+                // Llamamos a la función onComplete con la respuesta
+                onComplete(responseData)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                // Llamamos a la función onComplete con una cadena vacía en caso de error
+                onComplete("")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.android_project.ApiCalls.ApiCalls
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Instanciar ApiCalls
+        val apiCalls = ApiCalls()
+
+        // URL
+        val apiUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc"
+
+        // Referenciar el TextView en tu layout
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+
+        // Llamar a la funcion de la llamada de la api y gestionar la respuesta
+        apiCalls.fetchDataFromApi(apiUrl) { responseData ->
+            runOnUiThread {
+                // Actualizar el TextView con la respuesta o el mensaje de error
+                resultTextView.text = if (responseData.isNotEmpty()) {
+                    "Response from the API: $responseData"
+                } else {
+                    "Error calling the API"
+                }
+            }
+        }
     }
 }
