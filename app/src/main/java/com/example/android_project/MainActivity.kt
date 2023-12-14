c3d76e4a6c48ab594f4a08386fc2dc23d2025369
Ferran <marvenferry60@gmail.com>
2023-12-14 20:44:43 +0100

    Input con botón de buscar y botón de filtrar

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.android_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val changeViewButton: Button = findViewById(R.id.changeView)
+
+        changeViewButton.setOnClickListener {
+            val intent = Intent(this, Buscador::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
